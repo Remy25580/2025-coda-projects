1f15ef1d8144c4562346461813b9b23eb2fcde9f
Ryumi25580 <remy.martinot@coda-student.school>
2025-11-27 14:38:23 +0100

    feat: création de la page artists.php

---
+++ 004-php-lowify/app/artists.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once('inc/page.inc.php');
+require_once('inc/database.inc.php');
+
+try {
+    $db = new DatabaseManager(
+        dsn: "mysql:host=mysql;dbname=lowify;charset=utf8mb4",
+        username: "lowify",
+        password: "lowifypassword"
+    );
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$artists = [];
+$sql = <<<SQL
+SELECT *
+FROM artist;
+SQL;
+
+try{
+    $artists = $db->executeQuery($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$html = <<<HTML
+<body>
+<h1>Liste des Artistes</h1>
+HTML;
+
+$rawCSS = <<<CSS
+body{
+    background-color: darkslategrey;
+    font-family: Consolas;
+}
+h1{
+    margin-left: 25px;
+}
+.artists{
+    display: flex;
+    flex-wrap: wrap;
+}
+.artist{
+    padding: 8px;
+    border: 1px solid black;
+    display: inline-block;
+    margin-top: 70px;
+    margin-bottom: 70px;
+    margin-right: 100px;
+    margin-left: 100px;
+    
+}
+.artist img{
+    width: 250px;
+    height: auto;
+}
+.art-name{
+    font-size: 19px;
+}
+CSS;
+
+
+
+$page = new HTMLpage(title: "Lowify - Artistes");
+$page->addContent($html);
+$page->addRawStyle($rawCSS);
+
+$page->addContent(<<<HTML
+    <div class="artists">
+    HTML);
+foreach ($artists as $artist) {
+    $artistInfo = <<<HTML
+    <div class="artist">
+        <img src="{$artist["cover"]}" alt="cover de l'artiste {$artist["name"]}">
+        <div class="art-name">{$artist["name"]} </div>
+    </div>
+    <br>
+    HTML;
+
+    $page->addContent($artistInfo);
+}
+$page->addContent(<<<HTML
+    </div>
+    HTML);
+
+$page->addContent(<<<HTML
+    </body>
+    HTML);
+
+echo $page->render();
